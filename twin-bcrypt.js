30725c3720761c14090260da5159d51edba357db
fpirsch <fpirsch@free.fr>
2014-06-26 20:33:04 +0200

    Make the progress argument optional
    Improves compatability with the bcrypt package. See
    https://github.com/shaneGirish/bcrypt-nodejs/pull/14 and
    https://github.com/shaneGirish/bcrypt-nodejs/pull/21

--- twin-bcrypt.js
+++ twin-bcrypt.js
@@ -653,12 +653,13 @@
         /*
             data - [REQUIRED] - the data to be encrypted.
             salt - [REQUIRED] - the salt to be used to hash the password. if specified as a number then a salt will be generated and used (see examples).
-            progress - a callback to be called during the hash calculation to signify progress
+            progress - [OPTIONAL] - a callback to be called during the hash calculation to signify progress
             callback - [REQUIRED] - a callback to be fired once the data has been encrypted. uses eio making it asynchronous.
                 error - First parameter to the callback detailing any errors.
                 encrypted - Second parameter to the callback providing the encrypted form.
         */
-        if(!callback) {
+        callback = callback || progress;
+        if (!callback) {
             throw "No callback function was given.";
         }
         process.nextTick(function() {
