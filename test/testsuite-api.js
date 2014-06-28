c4b499f669ce0566973c32ebb763960d05d3a723
fpirsch <fpirsch@free.fr>
2014-06-28 23:11:06 +0200

    Add more tests

--- test/testsuite-api.js
+++ test/testsuite-api.js
@@ -1,5 +1,5 @@
 /* jshint expr: true */
-/* global describe, it, expect, TwinBcrypt */
+/* global chai, describe, it, expect, TwinBcrypt */
 
 describe('API test suite', function() {
 
@@ -23,56 +23,100 @@ describe('API test suite', function() {
         });
 
         it('should reject bad cost parameter', function() {
-            expect(function() { TwinBcrypt.genSaltSync("abc"); }).to.throw(Error, /cost|rounds/, "string");
-            expect(function() { TwinBcrypt.genSaltSync(-1); }).to.throw(Error, /cost|rounds/, "negative");
-            expect(function() { TwinBcrypt.genSaltSync(0); }).to.throw(Error, /cost|rounds/, "zero");
-            expect(function() { TwinBcrypt.genSaltSync(3); }).to.throw(Error, /cost|rounds/, "too low");
-            expect(function() { TwinBcrypt.genSaltSync(32); }).to.throw(Error, /cost|rounds/, "too high");
+            expect(function() { TwinBcrypt.genSaltSync("abc"); }).to.throw(Error, /cost|rounds/, 'string');
+            expect(function() { TwinBcrypt.genSaltSync(-1); }).to.throw(Error, /cost|rounds/, 'negative');
+            expect(function() { TwinBcrypt.genSaltSync(0); }).to.throw(Error, /cost|rounds/, 'zero');
+            expect(function() { TwinBcrypt.genSaltSync(3); }).to.throw(Error, /cost|rounds/, 'too low');
+            expect(function() { TwinBcrypt.genSaltSync(32); }).to.throw(Error, /cost|rounds/, 'too high');
         });
     });
-/*
+
     describe('Hash', function() {
-        it('should be generated asynchronously with a default cost parameter', function(done) {
-            TwinBcrypt.genSalt(function(error, result) {
-                expect(error).to.not.exist;
-                result.should.match(DEFAULT_SALT);
-                done();
+        var SALT = '$2a$04$......................',
+            HASH4 = '$2a$04$......................LAtw7/ohmmBAhnXqmkuIz83Rl5Qdjhm',
+            COST_4_HASH = /^\$2a\$04\$[.\/A-Za-z0-9]{21}[.Oeu][.\/A-Za-z0-9]{30}[.CGKOSWaeimquy2]$/,
+            DEFAULT_HASH = new RegExp('^\\$2a\\$'+TwinBcrypt.defaultCost+'\\$[./A-Za-z0-9]{21}[.Oeu][.\/A-Za-z0-9]{30}[.CGKOSWaeimquy2]$'),
+            noop = function() {};
+
+        describe('Synchronous', function() {
+            it('should warn when no password is given', function() {
+                expect(function() { TwinBcrypt.hashSync(); }).to.throw(Error);
+            });
+
+            it('should be generated with explicit salt', function() {
+                TwinBcrypt.hashSync('password', SALT).should.equal(HASH4);
+                TwinBcrypt.hashSync('', SALT).should.equal('$2a$04$......................w74bL5gU7LSJClZClCa.Pkz14aTv/XO');
+            });
+
+            it('should be generated with salt given as a number', function() {
+                TwinBcrypt.hashSync('password', 4).should.match(COST_4_HASH, 'as integer');
+                TwinBcrypt.hashSync('password', 4.8).should.match(COST_4_HASH, 'as float');
+                TwinBcrypt.hashSync('password', '4').should.match(COST_4_HASH, 'as string');
+            });
+
+            it('should be generated with default salt generation', function() {
+                TwinBcrypt.hashSync('password').should.match(DEFAULT_HASH);
             });
         });
 
-        it('should be generated asynchronously with an explicit cost parameter', function(done) {
-            TwinBcrypt.genSalt(4, function(error, result) {
-                expect(error).to.not.exist;
-                result.should.match(COST_4_SALT);
-                done();
-            });
-        });
-        
-        it('should reject a bad cost parameter in asynchrounous mode', function(done) {
-            TwinBcrypt.genSalt(32, function(error, result) {
-                error.should.match(/cost/);
-                expect(result).to.not.exist;
-                done();
-            });
-        });
 
-        it('should warn when no callback is given in asynchrounous mode', function() {
-            expect(function() { TwinBcrypt.genSalt(4); }).to.throw(Error);
-        });
+        describe('Asynchronous', function() {
+            it('should warn when no password is given', function() {
+                expect(function() { TwinBcrypt.hash(noop); }).to.throw(Error, /password|data/, '1 argument');
+                expect(function() { TwinBcrypt.hash(undefined, SALT, noop, noop); }).to.throw(Error, /password|data/, '4 arguments');
+            });
 
+            it('should warn when no callback is given', function() {
+                expect(function() { TwinBcrypt.hash('password', SALT); }).to.throw(Error, /callback/);
+            });
+
+
+            it('should be generated with an explicit salt', function(done) {
+                TwinBcrypt.hash('password', SALT, function(error, result) {
+                    expect(error).to.not.exist;
+                    result.should.equal(HASH4);
+                    done();
+                });
+                var spy = chai.spy(noop);
+                TwinBcrypt.hash('password', SALT, spy, function(error, result) {
+                    expect(error).to.not.exist;
+                    result.should.equal(HASH4);
+                    spy.should.have.been.called();
+                    done();
+                });
+            });
+
+            it('should be generated asynchronously with an explicit cost parameter', function(done) {
+                TwinBcrypt.hash('password', 4, function(error, result) {
+                    expect(error).to.not.exist;
+                    result.should.equal(HASH4);
+                    done();
+                });
+            });
+
+            it('should reject an invalid salt', function(done) {
+                TwinBcrypt.hash('password', '$2a$04$.............', function(error, result) {
+                    expect(error).to.match(/salt/);
+                    expect(result).to.not.exist;
+                    done();
+                });
+            });
+
+        });
     });
+
 
 /*
 hashSync(data, salt)
 
     data - [REQUIRED] - the data to be encrypted.
-    salt - [REQUIRED] - the salt to be used to hash the password. If specified as a number then a salt will be generated and used (see examples).
+    salt - [OPTIONAL] - the salt to be used to hash the password. If specified as a number then a salt will be generated and used (see examples).
 
 hash(data, salt, progress, cb)
 
     data - [REQUIRED] - the data to be encrypted.
-    salt - [REQUIRED] - the salt to be used to hash the password. If specified as a number then a salt will be generated and used (see examples).
-    progress - a callback to be called during the hash calculation to signify progress
+    salt - [OPTIONAL] - the salt to be used to hash the password. If specified as a number then a salt will be generated and used (see examples).
+    progress - [OPTIONAL] - a callback to be called during the hash calculation to signify progress
     callback - [REQUIRED] - a callback to be fired once the data has been encrypted.
         error - First parameter to the callback detailing any errors.
         result - Second parameter to the callback providing the encrypted form.
