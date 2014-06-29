465d7f7378ef67750e726b5ff61e55e82349169a
fpirsch <fpirsch@free.fr>
2014-06-29 11:40:45 +0200

    Fix spy tests.

--- test/testsuite-api.js
+++ test/testsuite-api.js
@@ -1,6 +1,10 @@
 /* jshint expr: true */
 /* global chai, describe, it, expect, TwinBcrypt */
 
+/**
+ * A trivial spy function.
+ * Also check https://github.com/chaijs/chai-spies
+ */
 function Spy() {
     var count = 0;
     var sp = function() {
@@ -19,8 +23,8 @@ function Spy() {
 }
 
 
-describe('API test suite', function() {
 
+describe('API test suite', function() {
 
     describe('Salt', function() {
         var COST_4_SALT = /^\$2a\$04\$[.\/A-Za-z0-9]{21}[.Oeu]$/;
@@ -30,7 +34,7 @@ describe('API test suite', function() {
             TwinBcrypt.defaultCost.should.be.within(10, 15);
         });
 
-        it('should be generated with explicit cost parameter', function() {
+        it('should accept explicit cost parameter', function() {
             TwinBcrypt.genSalt(4).should.match(COST_4_SALT, 'as integer');
             TwinBcrypt.genSalt(4.8).should.match(COST_4_SALT, 'as float');
             TwinBcrypt.genSalt('4').should.match(COST_4_SALT, 'as string');
@@ -50,9 +54,12 @@ describe('API test suite', function() {
     });
 
     describe('Hash', function() {
-        var SALT = '$2a$04$......................',
+        var SALT4 = '$2a$04$......................',
+            SALT7 = '$2a$07$......................',
             HASH4 = '$2a$04$......................LAtw7/ohmmBAhnXqmkuIz83Rl5Qdjhm',
+            HASH7 = '$2a$07$......................rkNUWThr5KSHevvQDxRDSYaalST.SGy',
             COST_4_HASH = /^\$2a\$04\$[.\/A-Za-z0-9]{21}[.Oeu][.\/A-Za-z0-9]{30}[.CGKOSWaeimquy26]$/,
+            COST_7_HASH = /^\$2a\$07\$[.\/A-Za-z0-9]{21}[.Oeu][.\/A-Za-z0-9]{30}[.CGKOSWaeimquy26]$/,
             DEFAULT_HASH = new RegExp('^\\$2a\\$'+TwinBcrypt.defaultCost+'\\$[./A-Za-z0-9]{21}[.Oeu][.\/A-Za-z0-9]{30}[.CGKOSWaeimquy26]$'),
             noop = function() {};
 
@@ -62,8 +69,8 @@ describe('API test suite', function() {
             });
 
             it('should be generated with explicit salt', function() {
-                TwinBcrypt.hashSync('password', SALT).should.equal(HASH4);
-                TwinBcrypt.hashSync('', SALT).should.equal('$2a$04$......................w74bL5gU7LSJClZClCa.Pkz14aTv/XO');
+                TwinBcrypt.hashSync('password', SALT4).should.equal(HASH4);
+                TwinBcrypt.hashSync('', SALT4).should.equal('$2a$04$......................w74bL5gU7LSJClZClCa.Pkz14aTv/XO');
             });
 
             it('should be generated with salt given as a number', function() {
@@ -84,50 +91,15 @@ describe('API test suite', function() {
             it('should warn when no password is given', function() {
                 expect(function() { TwinBcrypt.hash(noop); }).to.throw(Error, /password|data|argument/, '1 argument');
                 expect(function() { TwinBcrypt.hash(undefined, noop); }).to.throw(Error, /password|data|argument/, '2 arguments');
-                expect(function() { TwinBcrypt.hash(undefined, SALT, noop); }).to.throw(Error, /password|data|argument/, '3 arguments');
-                expect(function() { TwinBcrypt.hash(undefined, SALT, noop, noop); }).to.throw(Error, /password|data|argument/, '4 arguments');
+                expect(function() { TwinBcrypt.hash(undefined, SALT4, noop); }).to.throw(Error, /password|data|argument/, '3 arguments');
+                expect(function() { TwinBcrypt.hash(undefined, SALT4, noop, noop); }).to.throw(Error, /password|data|argument/, '4 arguments');
             });
 
             it('should warn when no callback is given', function() {
-                expect(function() { TwinBcrypt.hash('password', SALT); }).to.throw(Error, /callback/);
+                expect(function() { TwinBcrypt.hash('password', SALT4); }).to.throw(Error, /callback/);
             });
 
-            it('should be generated with (password, string, callback)', function(done) {
-                TwinBcrypt.hash('password', SALT, function(error, result) {
-                    expect(error).to.not.exist;
-                    result.should.equal(HASH4);
-                    done();
-                });
-                //var spy = chai.spy();
-                var spy = Spy();
-                TwinBcrypt.hash('password', SALT, spy, function(error, result) {
-                    expect(error).to.not.exist;
-                    result.should.equal(HASH4);
-                    spy.should.have.been.called();
-                    done();
-                });
-            });
-
-            it('should be generated with (password, progress, callback)', function(done) {
-                //var spy = chai.spy();
-                var spy = Spy();
-                TwinBcrypt.hash('password', noop, function(error, result) {
-                    expect(error).to.not.exist;
-                    result.should.match(DEFAULT_HASH);
-                    spy.should.have.been.called();
-                    done();
-                });
-            });
-
-            it('should be generated with (password, number, callback)', function(done) {
-                TwinBcrypt.hash('password', 4, function(error, result) {
-                    expect(error).to.not.exist;
-                    result.should.match(COST_4_HASH);
-                    done();
-                });
-            });
-
-            it('should be generated with (password, callback)', function(done) {
+            it('should accept (password, callback)', function(done) {
                 TwinBcrypt.hash('password', function(error, result) {
                     expect(error).to.not.exist;
                     result.should.match(DEFAULT_HASH);
@@ -135,16 +107,54 @@ describe('API test suite', function() {
                 });
             });
 
-            it('should warn when arguments are invalid', function() {
-                expect(function() { TwinBcrypt.hash('password', noop, noop, SALT); }).to.throw(Error, /salt|callback|argument/);
+            it('should accept (password, string_salt, callback)', function(done) {
+                TwinBcrypt.hash('password', SALT4, function(error, result) {
+                    expect(error).to.not.exist;
+                    result.should.equal(HASH4);
+                    done();
+                });
             });
-
-            it('should be generated with an explicit cost parameter', function(done) {
+            
+            it('should accept (password, number_salt, callback)', function(done) {
                 TwinBcrypt.hash('password', 4, function(error, result) {
                     expect(error).to.not.exist;
                     result.should.match(COST_4_HASH);
                     done();
                 });
+            });
+
+            it('should accept (password, progress, callback)', function(done) {
+                var spy = Spy();
+                TwinBcrypt.hash('password', spy, function(error, result) {
+                    expect(error).to.not.exist;
+                    result.should.match(DEFAULT_HASH);
+                    spy.should.have.been.called();
+                    done();
+                });
+            });
+
+            it('should accept (password, string_salt, progress, callback)', function(done) {
+                var spy = Spy();
+                TwinBcrypt.hash('password', SALT7, spy, function(error, result) {
+                    expect(error).to.not.exist;
+                    result.should.equal(HASH7);
+                    spy.should.have.been.called();
+                    done();
+                });
+            });
+
+            it('should accept (password, number_salt, progress, callback)', function(done) {
+                var spy = Spy();
+                TwinBcrypt.hash('password', 7, spy, function(error, result) {
+                    expect(error).to.not.exist;
+                    result.should.match(COST_7_HASH);
+                    spy.should.have.been.called();
+                    done();
+                });
+            });
+
+            it('should warn when arguments are invalid', function() {
+                expect(function() { TwinBcrypt.hash('password', noop, noop, SALT4); }).to.throw(Error, /salt|callback|argument/);
             });
 
             it('should reject an invalid salt', function(done) {
