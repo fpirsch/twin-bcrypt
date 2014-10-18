47f6c19accde03e117273768ead8e4578d831278
fpirsch <francois@pirsch.fr>
2014-10-18 14:21:13 +0200

    Display version number

--- demo/demo.js
+++ demo/demo.js
@@ -6,6 +6,7 @@ buttons[0].onclick = clickButton;
 buttons[1].onclick = clickButton;
 buttons[2].onclick = clickButton;
 
+document.getElementById('version').textContent = 'v' + TwinBcrypt.version;
 
 function clickButton(event) {
     var button = event.target,
